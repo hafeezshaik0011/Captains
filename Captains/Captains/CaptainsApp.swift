b5aab3433c0ffbfa55bf8c20670a236627e13386
hafeezshaik0011 <shaik.hafeez4@gmail.com>
2025-08-27 12:37:45 +0530

    Project creation

---
+++ Captains/Captains/CaptainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptainsApp.swift
+//  Captains
+//
+//  Created by hafeez Sheik on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Captains/Captains/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Captains
+//
+//  Created by hafeez Sheik on 27/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
